ebfcf9446efd3c25d31c9d5a12ce0cbec44eb50d
Kiesy Daniel <57769796+KeisyNKK@users.noreply.github.com>
2019-11-17 03:47:09 -0300

    main.c

--- main.c
+++ main.c
@@ -1,35 +1,48 @@
+#include<stdlib.h>
 #include<stdio.h>
 #include "Matrix_creator.h"
-#include<stdlib.h>
-struct Play {
-    int ID;
-    float  Distance;
-
-};
 
 
 int main(){
-    struct Play *zombie=NULL;
-    FILE *conf;
-    conf = fopen("config.txt","r");
-     if (conf == NULL){
-       printf("ERRO! O arquivo 'config.txt' de path: nao foi aberto!\n");
-       system("exit");
-     }
 
-    int size=0, z=0;
-    char ** matrix;
+	/*Etapa de inicialização e criação da matriz*/
 
-    itialization(conf,&size, &z);
+	FILE *conf;
 
-    zombie = (struct Play *)malloc(sizeof(struct Play)*z);
+	conf = fopen("config.txt","r");
 
-    matrix = creator(size, conf,&zombie);
-    zombie[0].ID =50;
-    printf("zombie[0].ID = %d", zombie[0].ID);
+		if (conf == NULL)
+		{
+			printf("ERRO! O arquivo 'config.txt' de path: nao foi aberto!\n");
+			system("exit");
+		}
 
-    draw(size,matrix);
 
-    return 0;
-    }
+	int size = 0, z = 0;
 
+	char ** matrix;
+	
+	Play person;
+	Play *zombie = NULL;
+
+	zombie = initialization(conf, &size, &z);
+
+	matrix = creator(size, conf, zombie, z, &person);
+	
+	fclose(conf);
+	
+	//loop
+
+	int turnos = 0;
+
+		do{
+			system("clear");
+			draw(size,matrix);
+			move(matrix, zombie, &person,z);
+		
+			turnos++;
+
+		}while(1);
+
+	return 0;
+}
