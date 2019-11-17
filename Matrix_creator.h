8600bf47024a48b0898cea9067e4797375e1b1d4
Kiesy Daniel <57769796+KeisyNKK@users.noreply.github.com>
2019-11-17 03:48:13 -0300

    Matrix_creator.h

--- Matrix_creator.h
+++ Matrix_creator.h
@@ -1,8 +1,16 @@
 #ifndef MATRIX_CREATOR_H_INCLUDED
 #define MATRIX_CREATOR_H_INCLUDED
 
-void itialization(FILE *conf,int *size, int *z);
-char **creator(int size,FILE *conf, struct Play *zombies);
-void draw(int size, char **matrix);
 
+typedef struct 
+{
+    int ID, xi, yi,xf,yf;
+    float Distance;
+
+}Play;
+
+Play *initialization(FILE *conf,int *size, int *z);
+char **creator(int size,FILE *conf, Play *zombies, int z, Play *person);
+void draw(int size, char **matrix);
+void move(char **matrix,  Play *zombie, Play *person, int z);
 #endif // MATRIX_CREATOR_H_INCLUDED
