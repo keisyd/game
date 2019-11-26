aae4fde523ec5b8bec912ad36eff63fb3f623efe
Kiesy Daniel <57769796+KeisyNKK@users.noreply.github.com>
2019-11-26 13:10:46 -0300

    Matrix_creator.h

--- Matrix_creator.h
+++ Matrix_creator.h
@@ -2,20 +2,21 @@
 #define MATRIX_CREATOR_H_INCLUDED
 
 
-
 typedef struct 
 {
     int ID, xi, yi,xf,yf; // initial and final position
     int px, py;	//previous position
     float Distance;
+    char PlayerName[20];
 
 }Play;
 
-Play *initialization(FILE *conf,int *size, int *z);
-char **creator(int size,FILE *conf, Play *zombies, int z, Play *person);
-void draw(int size, char **matrix);
-void move(char **matrix,  Play *zombie, Play *person, int z);
-void analysis(Play person, Play *zombie, int z, int condition);
+Play *initialization(int *size, int *z, char **argv, int *canGo, Play *person);
+char **creator(int size, Play *zombies, int z, Play *person);
+void draw(int size, char **matrix, int *turns);
+void moveZombie(char **matrix, Play *zombie, Play *person, int z);
+void movePerson(char **matrix, Play *person, int *canGo);
+void analysis(Play person, Play *zombie, int z, int condition, int turns);
 int verify(Play *zombie, Play person, int size, int z, int *condition);
 
 
