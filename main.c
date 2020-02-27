19475040eaa934e7d01b609d07b31c524ec7a9eb
Kiesy Daniel <57769796+KeisyNKK@users.noreply.github.com>
2020-02-27 20:31:21 -0300

    main.c
    Final Version

--- main.c
+++ main.c
@@ -9,8 +9,6 @@ int main(int argc, char *argv[]){
 
 	/*Etapa de inicialização e criação da matriz*/
 
-
-
 	int size = 0, z = 0,canGo=0;
 
 	char ** matrix;
@@ -24,14 +22,13 @@ int main(int argc, char *argv[]){
 	if(canGo)
 		return 0;
 
-
-
 	matrix = creator(size, zombie, z, &person);	
 	
 	
 	//loop
 	
-		int turns = 2, condition; //turns é 2 para o primeiro ciclo correr bem sem perda de informação ao final do último ciclo, 
+		int turns = 2, condition; 
+		//turns é 2 para o primeiro ciclo correr bem sem perda de informação ao final do último ciclo, 
 		canGo = 0;
 		//desenho do primeiro ciclo
 		system("clear"); 
@@ -40,25 +37,26 @@ int main(int argc, char *argv[]){
 
 		while(verify(zombie, person, size, z, &condition))
 		{
-		
-			moveZombie(matrix, zombie, &person, z);//movendo zombies
-			
+					
 			do//escolha uma letra até que alguma letra valida seja escolhida
 			{
-				movePerson(matrix, &person, &canGo);
+				movePerson(matrix, &person, &canGo, zombie, z);
 				
 				if(canGo)	
 					printf("\nEscolha uma letra valida"); 
 
 			}while(canGo);
 			
+			moveZombie(matrix, zombie, &person, z);//movendo zombies
+
 			system("clear"); //limpar a tela
 			
 			draw(size,matrix, &turns); //desenhar a matriz movida
 
 			turns++;
-		}
 
+		}
+	
 	analysis(person, zombie, z, condition, turns);
 	
 	return 0;
